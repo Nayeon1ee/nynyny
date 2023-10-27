d5dc6bd26388ba43feff41325d6dd31d4144d656
lny <leenayeon5127@gmail.com>
2023-10-27 11:38:20 +0900

    nayeon_commit_1027_1138_화면 커밋

--- pro01/src/main/java/com/spring/pro/controller/MyController.java
+++ pro01/src/main/java/com/spring/pro/controller/MyController.java
@@ -1,20 +1,20 @@
 package com.spring.pro.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class MyController {
 
-	@GetMapping("/test01")
+//	@GetMapping("/test01")
 	public String calendar() {
 		System.out.println("test01 호출");
 		
 		return "test01";
 	}
 	
-	@GetMapping("/exam")
+//	@GetMapping("/exam")
 	public String exam() {
 		System.out.println("exam 달력 호출");
 		return "background-events";
